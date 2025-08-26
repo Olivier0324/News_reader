aeb7c0fc0814a43d9361366ca238423568d141c4
Olivier0324 <techies.dev24@gmail.com>
2025-08-26 14:19:20 +0200

    Update: i have updated navbar by creating news reader logo using lucide-react icon and tailwind css styling

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom";
 import ThemeToggle from "./ThemeToggle";
+import { BookTextIcon } from "lucide-react";
 
 export default function Navbar() {
   return (
     <nav className="flex justify-between items-center px-6 py-4 
                     bg-blue-700 dark:bg-gray-800 shadow">
-      <Link to="/" className="text-xl font-bold text-gray-900 dark:text-white">
-        News Reader
+      <Link to="/" className="text-xl font-bold text-gray-900 dark:text-white flex items-center gap-2 bg-gray-500 py-2 px-4 rounded">
+        <BookTextIcon/> News Reader
       </Link>
       <ThemeToggle />
     </nav>
